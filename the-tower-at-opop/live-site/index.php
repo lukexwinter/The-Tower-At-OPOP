c45ae3e851c56a093da5d734e67d86a8e7ef3544
Luke Sedmak <luke@bluestonecreative.com>
2014-02-13 17:51:12 -0500

    start of mobile for header / footer
    Complete mobile menu button click function and design mobile footer.
    Use fittext plugin for text?

--- the-tower-at-opop/live-site/index.php
+++ the-tower-at-opop/live-site/index.php
@@ -27,11 +27,18 @@ define("MAINURL", "http://opoptower.localhost.com/missouri/st-louis/the-tower-at
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
-        <header>
+        <header class="collapsed">
 			<nav>
 				<a href=""><img id="tower-opop-logo" width="258" height="59" src="<?php echo MAINURL."img/tower-opop-logo.png"; ?>" alt="The Tower - OPOP" /></a>
 				<ul>
-					<li class="tower-logo"><a href=""><img width="163" height="16" src="<?php echo MAINURL."img/tower-logo.png"; ?>" alt="The Tower" /></a></li>
+					<li class="menu-button">
+						<a href="">
+							<span style="margin-top: 4px;"></span>
+							<span></span>
+							<span style="margin: 0;"></span>
+						</a>
+					</li>
+					<li class="tower-logo"><a href=""><img width="163" height="38" src="<?php echo MAINURL."img/tower-opop-logo.png"; ?>" alt="The Tower" /></a></li>
 					<li><a href="">Apartments</a></li>
 					<li><a href="">Penthouses</a></li>
 					<li><a href="">The Tower</a></li>
@@ -53,7 +60,6 @@ define("MAINURL", "http://opoptower.localhost.com/missouri/st-louis/the-tower-at
 
 		<section id="apartments" class="scrollsections">
 			<article>
-				<h6>Limited Availability</h6>
 				<h2>25 Floors,<br /> 126 luxury apartments</h2>
 				<p>As a V.I.P member, be the first to stake claim on an incredible view.</p>
 				<a href="" class="button">View Apartments</a>
@@ -62,7 +68,6 @@ define("MAINURL", "http://opoptower.localhost.com/missouri/st-louis/the-tower-at
 
 		<section id="amenities" class="scrollsections">
 			<article>
-				<h6>Amenities</h6>
 				<h2>State-of-the-art fitness center &amp; club room</h2>
 				<p>Floors 24-25 let you look out, hang out, or work out. The sky is the limit.</p>
 				<a href="" class="button">View Amenities</a>
@@ -71,7 +76,6 @@ define("MAINURL", "http://opoptower.localhost.com/missouri/st-louis/the-tower-at
 
 		<section id="location" class="scrollsections">
 			<article>
-				<h6>Location</h6>
 				<h2>Located in<br/> downtown St. Louis</h2>
 				<p>Never skip a beat. Experience the heart of  nightlife & entertainment.</p>
 				<a href="" class="button">View Location</a>
@@ -80,15 +84,66 @@ define("MAINURL", "http://opoptower.localhost.com/missouri/st-louis/the-tower-at
 
 		<section id="perks" class="scrollsections">
 			<article>
-				<h6>V.I.P. Perks</h6>
 				<h2>Take the high life<br /> on the go.</h2>
 				<p>Our residents are V.I.P. Take advantage of valet parking, and perks.</p>
 				<a href="" class="button">Learn More</a>
 			</article>
 		</section>
 		
-		<footer class="scrollsections" style="background: red; height: 500px;">
-			
+		<footer id="footer" class="scrollsections">
+			<div class="content top clearfix">
+				<div class="col5">
+					<img src="<?php echo MAINURL."img/opop-footer-logo.png"; ?>" width="88" height="28" alt="OPOP" />
+					<p>
+						The Tower at OPOP<br />
+						901 Locust Street St. Louis, MO 63101<br /> 
+						<br />
+						314 621 5443<br />
+						<a href="mailto:info@thetoweratopop.com">info@thetoweratopop.com</a>
+					</p>
+				</div>
+				<div class="col3">
+					<h6>Site Map</h6>
+					<ul>
+						<li><a href="">Apartments</a></li>
+						<li><a href="">Penthouses</a></li>
+						<li><a href="">The Tower</a></li>
+						<li><a href="">Location</a></li>
+						<li><a href="">V.I.P. Perks</a></li>
+					</ul>
+					<ul>
+						<li><a href="">Amenities</a></li>
+						<li><a href="">Photo Gallery</a></li>
+						<li><a href="">Schedule a Tour</a></li>
+						<li><a href="">Contact</a></li>
+					</ul>
+				</div>
+				<div class="col2">
+					<h6>Social</h6>
+					<ul>
+						<li><a href="">Twitter</a></li>
+						<li><a href="">Facebook</a></li>
+						<li><a href="">Pinterest</a></li>
+						<li><a href="">Google +</a></li>
+						<li><a href="">Instagram</a></li>
+						<li><a href="">Youtube</a></li>
+					</ul>
+				</div>
+				<div class="col2 last">
+					<h6>Residents</h6>
+					<ul>
+						<li><a href="">napLife</a></li>
+						<li><a href="">Sign Out</a></li>
+					</ul>
+				</div>
+			</div>
+			<div style="clear: both;"></div>
+			<div class="bottom">
+				<div class="content">
+					<p id="breadcrumbs"><a href="http://apartments.naproperties.com/" title="North American Properties">North American Properties</a><span class="divider"> / </span><a href="http://apartments.naproperties.com/missouri" title="Missouri Apartments">Missouri Apartments</a><span class="divider"> / </span><a href="http://apartments.naproperties.com/missouri/st-louis" title="St. Louis Apartments">St. Louis Apartments</a><span class="divider"> / </span><a href="<?php echo MAINURL; ?>" title="The Tower at OPOP">The Tower at OPOP</a><?php echo $breadcrumb; ?></p>
+					<p id="terms">Copyright &copy; 2013 North American Properties. All rights reserved. <a class="gray" href="<?php echo MAINURL."privacy-policy"; ?>">Privacy Policy</a>  |  <a class="gray" href="<?php echo MAINURL."terms-and-conditions"; ?>">Terms &amp; Conditions</a></p>
+				</div>
+			</div>
 		</footer>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
