6d3f2ad0a9cb39d98255460c204937ee2bba0160
Luke Sedmak <luke@bluestonecreative.com>
2014-02-26 17:42:55 -0500

    midway point of tower page
    midway point of tower page

--- the-tower-at-opop/live-site/about.php
+++ the-tower-at-opop/live-site/about.php
@@ -1,17 +1,68 @@
 <?php include($_SERVER['DOCUMENT_ROOT']."/the-tower-at-opop/live-site/template/header.inc.php"); ?>
-		<section class="about">
+<div id="skrollr-body">
+		<section class="about general">
 			<nav class="nav2">
 				<div class="content clearfix">
-					<a href="">Location</a>
+					<a class="first" href="">Location</a>
 					<a href="">Amenities</a>
 					<a href="">V.I.P. Perks</a>
 				</div>
 			</nav>
 			
+			<div class="header-image about"></div>
 			
-		</section>
+			<div class="content">
+				<article class="intro clearfix">
+					<h1 class="col4">Breathtaking views from high above Old Post Office Plaza</h1>
+					<p class="col3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis massa sit amet dolor viverra vestibulum. Integer luctus porttitor orci vel semper. Vivamus in dapibus mauris. Curabitur aliquet interdum est, ac dictum risus pharetra eu. Vestibulum semper consequat urna, vitae condimentum felis tincidunt nec. Aliquam lacus leo, rutrum a convallis ut, gravida sed erat. Praesent egestas mi quis quam ornare, eget blandit ipsum egestas.</p>
+					<p class="col3">Pellentesque vehicula vel lectus sed gravida. Nam pulvinar elementum arcu, ac euismod dolor condimentum ullamcorper. Vivamus at ante ultrices libero sodales viverra. Aliquam rhoncus pellentesque tempor. Vivamus luctus est nisl, a elementum lacus mollis at. Vivamus sit amet lacinia est. Quisque porta diam eget interdum lacinia. Proin in arcu ac risus facilisis blandit. Duis volutpat velit ipsum, ac consectetur purus molestie vel. Mauris scelerisque malesuada ante, et tempor libero sagittis ac. Donec a iaculis lacus.</p>
+				</article>
+								
+				<article class="flr24-25 clearfix">
+					<h2>Floors 24-25</h2>
+					<div class="col7">
+						<img src="<?php echo MAINURL."live-site/img/about-penthouse.jpg"; ?>" alt="" />
+					</div>
+					<div class="col4">
+						<div style="position: relative;">
+							<img data-0="width: 1px; height: 1px;" data-center-top="width: 266px; height: 266px; z-index: 2; position: relative;" class="skrollable skrollable-between" src="<?php echo MAINURL."live-site/img/penthouses-circle.png"; ?>" alt="" />
+							<span style="background-color: #6aa3bc; height: 3px; width: 500px; top: 50%; display: block; position: absolute; left: 0; z-index: 1;" data-0="width: 1px; left: 100px; opacity: 0;" data-center-top="width: 250px; left: 300px; opacity: 1;"><span style="background-color: #6aa3bc; width: 9px; height: 9px; display: block; position: absolute; right: 0; top: -3px; -moz-border-radius: 50px; -webkit-border-radius: 50px; border-radius: 50px;"></span></span>
+						</div>
+						<p>
+							2 Penthouses<br /><br />
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis neque eget elit fermentum placerat. Duis feugiat gravida justo, vel mattis turpis eleifend vel. Aenean. <br /><br />
+							<a href="">View Penthouses</a>
+						</p>
+					</div>
+					<div style="clear: both; height: 100px;"></div>
+					
+					<div class="col7">
+						<img src="<?php echo MAINURL."live-site/img/about-fitness.jpg"; ?>" alt="" />
+					</div>
+					<div class="col4">
+						<div style="position: relative;">
+							<img data-0="width: 1px; height: 1px;" data-center-top="width: 266px; height: 266px; z-index: 2; position: relative;" src="<?php echo MAINURL."live-site/img/fitness-circle.png"; ?>" alt="" />
+							<span style="background-color: #6aa3bc; height: 3px; width: 500px; top: 50%; display: block; position: absolute; left: 0; z-index: 1;" data-0="width: 1px; left: 100px; opacity: 0;" data-center-top="width: 250px; left: 300px; opacity: 1;"><span style="background-color: #6aa3bc; width: 9px; height: 9px; display: block; position: absolute; right: 0; top: -3px; -moz-border-radius: 50px; -webkit-border-radius: 50px; border-radius: 50px;"></span></span>
+						</div>
+						<p>
+							2 Penthouses<br /><br />
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis neque eget elit fermentum placerat. Duis feugiat gravida justo, vel mattis turpis eleifend vel. Aenean. <br /><br />
+							<a href="">View Penthouses</a>
+						</p>
+					</div>
+				</article>
+			</div>
 			
-		
-		
+			
+			
+		</section>			
+</div>		
+
+<script>
+	$(window).load(function(){
+		var s = skrollr.init();
+	});
+</script>
+<script src="<?php echo MAINURL."live-site/js/skrollr.min.js"; ?>"></script>		
 
 <?php include($_SERVER['DOCUMENT_ROOT']."/the-tower-at-opop/live-site/template/footer.inc.php"); ?>
