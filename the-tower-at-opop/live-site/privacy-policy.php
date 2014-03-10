91cc887a22f4f27f746daff65b516f4c2384722b
Luke Sedmak <luke@bluestonecreative.com>
2014-03-10 09:34:32 -0400

    synch with home computer version
    synch with home computer version

---
+++ the-tower-at-opop/live-site/privacy-policy.php
@@ -0,0 +1,29 @@
+<?php include($_SERVER['DOCUMENT_ROOT']."/the-tower-at-opop/live-site/template/header.inc.php"); ?>
+<section class="general">
+	<div class="content">
+	    <h1>The Tower Privacy Policy</h1>
+	    <h4>Effective Date: December 1, 2014</h4>
+	    <p>North American Properties (NAP) is committed to protecting the privacy of its Customers' personal information while providing the most useful and enjoyable Web experience possible. This Privacy Statement applies with respect to the information that NAP collects from the company Website, located at www.TheTowerAtOpop.com unless stated otherwise. NAP does not collect and keep personal information unless it is volunteered.</p>
+	    <p>Please review this Privacy Statement periodically as NAP may change or add to this statement. Amendments to this statement will be posted at www.napCincinnati.com and will be effective when posted. A customer's continued use of this site following the posting of any amendment, modification or change shall constitute their acceptance thereof. If there are any questions or concerns about privacy, please do not hesitate to contact NAP. Our contact information is:</p>
+	    <p>North American Properties<br>
+	      901 Locust Street <br>
+	      St. Louis, MO 63101<br>
+	      314-621-5443<br>
+	      Website: <a href="http://www.thetoweratopop.com">www.TheTowerAtOpop.com</a></p>
+	    <h4>Our Commitment to Security</h4>
+	    <p>NAP has industry standard security measures in place to protect the loss, misuse and alteration of the personal information under NAP's control. While there is no such thing as "perfect security" on the Internet, NAP will take all reasonable steps to ensure the safety of Customers' personal information.</p>
+	    <h4>Personal Information NAP Collects, How it is Used and Opting Out</h4>
+	    <p>NAP only collects personal information that is voluntarily provided such as through Online Leasing, napLife and by contacting NAP by email or online form with questions, comments or inquiries. Personal information includes things such as social security number, email, phone number, address or personal financial information such as credit card numbers. Many features of TheTowerAtOpop.com may be used anonymously; customers will have to supply the required information to gain access to some of the premium services such as Online Leasing and napLife.</p>
+	    <p>NAP may contact customers to provide leasing information or other related data. Customers who supply NAP with their contact information may receive periodic mailings or email from NAP with information about NAP. If a user does not wish to receive these mailings, please contact NAP at the address listed above. Please provide your name and address so NAP can ensure that you are removed from the mailing list.</p>
+	    <p>Customers who do not wish to receive email messages may either contact NAP at the address listed above or follow the instructions included in the email received. Please provide your name and email address so NAP can ensure that you are removed from the emailing list.</p>
+	    <p>NAP uses a contact form on its site so that customers can contact NAP with their questions or concerns. NAP will read every message and try to respond promptly. NAP may save email comments to improve its website, services and products.</p>
+	    <h4>Site Traffic Monitoring</h4>
+	    <p>Like most sites, this site does collect navigational information such as Internet Protocol (IP) address, Internet Service Provider (ISP), browser type, linking information, what Web page(s) were visited on the www.TheTowerAtOpop.com site and duration of the visit to those page(s). This information is not linked in any way to personal information that is otherwise collected. This information is only used to improve NAP's Website and make the user's access to the site is convenient and efficient.</p>
+	    <h4>Information NAP Provides to Third Parties</h4>
+	    <p>NAP will not share customer's personal information collected online or off-line with any third party for direct marketing purposes. NAP provides other companies access to personal information if necessary to allow them to provide database or server maintenance or security, and to help NAP process inquiries, payments and requests. NAP may also disclose personal information if it has a good faith belief that such action is necessary to comply with a current judicial proceeding, a court order, legal process or legal request.</p>
+	    <h4>Sale of Company Assets</h4>
+	    <p>The compilation of information collected through NAP's site is considered a trade secret of NAP's. As the owner of such information, NAP may disclose, transfer or sell the information as an asset of the company in conjunction with due diligence for or completion of a merger, reorganization or sale to a third party of our company or a major portion of NAP's assets. Although NAP does not currently anticipate such an event, changes to this Privacy Statement might result from changes in NAP corporate status.</p>
+	</div>
+</section>
+<?php include($_SERVER['DOCUMENT_ROOT']."/the-tower-at-opop/live-site/template/footer.inc.php"); ?>
+     
